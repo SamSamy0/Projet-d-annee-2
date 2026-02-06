8251f9d02d08864769f1b7169251b20fc2d973b3
Samy <scontuliano@gmail.com>
2026-02-06 15:41:51 +0100

    Structure for login and signup
    Beginning of how to implement login and signup function

--- src/displayWindow.cpp
+++ src/displayWindow.cpp
@@ -3,6 +3,31 @@
 #include <TGUI/Backend/SFML-Graphics.hpp>
 #include <TGUI/TGUI.hpp>
 
+void Window::signIn(tgui::EditBox::Ptr usrname, tgui::EditBox::Ptr pswd) {
+  bool newUsr = true;
+  for (int i = 0; i < DB.size(); i++) {
+    if (DB[i][0] == "bonjour") {
+    }
+  }
+}
+void Window::login(tgui::EditBox::Ptr usrname, tgui::EditBox::Ptr pswd) {
+  /* Envoyer l'identifiant et le password à la base de donnée.
+   * On vérifie que la connexion est correct -> autre fonction
+   *
+   * Tant qu'on est pas connecté, on reste sur la page de connexion
+   * et un message est affiché
+   */
+  while (Window::checkLogin()) {
+    // On affiche en boucle le menu de connexion
+    // Window::menuWindow();
+    usrname->setDefaultText("Wrong identifier(s)");
+    gui.draw();
+  }
+  /* Si on est connecté, le menu est affiché avec les projets.
+   * */
+  Window::menuWindow();
+}
+
 void Window::initWidget() {
   updateTextSize();
   gui.onViewChange([this] { updateTextSize(); });
@@ -14,15 +39,17 @@ void Window::initWidget() {
   gui.add(editBoxUsername, "Username");
 
   auto editBoxPassword = tgui::EditBox::copy(editBoxUsername);
-  editBoxUsername->setPosition({"37.5%", "43,75%"});
-  editBoxUsername->setDefaultText("Password...");
-  editBoxUsername->setPasswordCharacter('*');
+  editBoxPassword->setPosition({"37.5%", "43,75%"});
+  editBoxPassword->setDefaultText("Password...");
+  editBoxPassword->setPasswordCharacter('*');
   gui.add(editBoxPassword, "Password");
 
   auto loginButton = tgui::Button::create("Login");
   loginButton->setPosition({"25%", "53.125%"});
   loginButton->setSize({"12.5%", "3.125%"});
   gui.add(loginButton);
+
+  loginButton->onPress(&Window::login, this, editBoxUsername, editBoxPassword);
 
   auto signInButton = tgui::Button::create("Sign in");
   signInButton->setPosition({"62.5%", "53.125%"});
@@ -36,6 +63,7 @@ void Window::updateTextSize() {
 void Window::processEvents() {
   while (const std::optional event = mainWindow.pollEvent()) {
     gui.handleEvent(*event);
+    // Close the Window
     if (event->is<sf::Event::Closed>())
       mainWindow.close();
 
@@ -49,7 +77,7 @@ void Window::processEvents() {
 
 Window::Window()
     : mainWindow(sf::VideoMode::getDesktopMode(),
-                 "SFML works!",
+                 "Game name",
                  sf::State::Fullscreen),
       gui(mainWindow) {
   initWidget();
@@ -58,13 +86,6 @@ void Window::run() {
   while (mainWindow.isOpen()) {
     processEvents();
     mainWindow.clear(sf::Color::White);
-    gui.draw();
     mainWindow.display();
   }
 }
-
-// int main() {
-//   Window window;
-//   window.run();
-//   return 0;
-// }
--- src/displayWindow.hpp
+++ src/displayWindow.hpp
@@ -3,13 +3,20 @@
 #include <SFML/Graphics.hpp>
 #include <TGUI/Backend/SFML-Graphics.hpp>
 #include <TGUI/TGUI.hpp>
+#include <unordered_map>
 class Window {
  private:
+  std::unordered_map<std::string, std::string> DB;
+  bool isLoggedIn;
   sf::RenderWindow mainWindow;
   tgui::Gui gui;
   void initWidget();
   void updateTextSize();
   void processEvents();
+  void login(tgui::EditBox::Ptr usrname, tgui::EditBox::Ptr pswd);
+  void signIn(tgui::EditBox::Ptr usrname, tgui::EditBox::Ptr pswd);
+  void menuWindow();
+  bool checkLogin();
 
  public:
   Window();
