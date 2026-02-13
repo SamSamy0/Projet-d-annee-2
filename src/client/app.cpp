c49e1b4b59b5dbc5ccc8de7ea64b59e967678d0c
itsadoums <kanounadampro@gmail.com>
2026-02-13 02:09:45 +0100

    ajout de la classe App et préparation des fichiers à venir

---
+++ src/client/app.cpp
@@ -0,0 +1,11 @@
+#include "app.hpp"
+
+using namespace std;
+
+void App::run() {
+    // connexion avec le serveur
+
+    // ecran d'inscription et connexion, boucle tant qu'il s'est pas connecté
+
+    // boucle principale qui appele les classes de home et de l'éditeur dse carte
+}
---
+++ src/client/app.hpp
@@ -0,0 +1,10 @@
+#pragma once
+#include <SFML/Graphics.hpp>
+
+using namespace std;
+
+class App {
+    MainView projectView_; // l'éditeur de carte, faut que je rajoute mon fichier pour éviter l'arreur, je le ferais quand carte sera 100% opérationnel
+    public :
+        void run();
+};
