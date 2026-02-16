391c8aa9e5580ed9744e83c71aa08236753712e1
itsadoums <kanounadampro@gmail.com>
2026-02-16 23:40:44 +0100

    résolution bug de pull

--- src/project.cpp
+++ src/project.cpp
@@ -4,21 +4,9 @@
 #include "tool.hpp"
 #include <memory>
 
-
-
-<<<<<<< HEAD
-Project::Project(unsigned int scale, sf::Vector2u size, sf::RenderWindow &window) : window_{window} {
-
-  map_ = std::make_shared<Map>(1,size,scale);
-  tools_.push_back(std::make_shared<PixelBrush>(map_));
-  tools_.push_back(std::make_shared<PixelShift>(map_));
-
-}
-=======
-Project::Project(unsigned int scale, sf::Vector2u size): map_{std::make_shared<Map>(1,size,scale)},
+Project::Project(unsigned int scale, sf::Vector2u size, sf::RenderWindow &window): window_{window}, map_{std::make_shared<Map>(1,size,scale)},
   toolbar_{map_}{}
 
->>>>>>> a2ad2e28b449ff6dc02f111fa1fb80e2395bd9e9
 unsigned int Project::getScale() { return map_->getScale(); }
 std::shared_ptr<Map> Project::getMap(){return map_;}
 ToolBar Project::getToolBar(){return toolbar_;}
