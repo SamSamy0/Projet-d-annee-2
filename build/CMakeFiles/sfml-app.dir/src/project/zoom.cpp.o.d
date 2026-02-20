f696cfb5a7d1c80958b25c62f46db43ba213e362
Samy <scontuliano@gmail.com>
2026-02-20 15:37:32 +0100

    Merging with main

--- build/CMakeFiles/sfml-app.dir/src/project/zoom.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/sfml-app.dir/src/project/zoom.cpp.o: \
-  /Users/itsadoums/dev/projet/projet_annee/group-13/src/project/zoom.cpp \
-  /Users/itsadoums/dev/projet/projet_annee/group-13/src/project/zoom.hpp
--- src/Window/displayWindow.cpp
+++ src/Window/displayWindow.cpp
@@ -373,26 +373,37 @@ void Window::processEvents() {
         if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::LSystem) ||
             sf::Keyboard::isKeyPressed(sf::Keyboard::Key::LControl)) {
           // toolType tool = project->getToolBar().getSelected();
-          // if(tool == PIXELBRUSH || tool == SPRITEBRUSH){
+          // if (tool == PIXELBRUSH || tool == SPRITEBRUSH) {
           //   sf::Vector2u size =
-          //   project->getToolBar().getSelectedTool()->getSize(); float sizex =
-          //   size.x; float sizey = size.y; sf::Vector2u mapSize =
-          //   project->getMap()->getSize(); float step =
-          //   std::min(mapSize.x,mapSize.y)/100.0f;
+          //       project->getToolBar().getSelectedTool()->getSize();
+          //   float sizex = size.x;
+          //   float sizey = size.y;
+          //   sf::Vector2u mapSize = project->getMap()->getSize();
+          //   float step = std::min(mapSize.x, mapSize.y) / 100.0f;
           //
-          //   if (wheelEvent->delta > 0){sizex += 1; sizey += 1;}
-          //   else if(wheelEvent->delta <0){sizex -= 1 ; sizey -= 1;}
+          //   if (wheelEvent->delta > 0) {
+          //     sizex += 1;
+          //     sizey += 1;
+          //   } else if (wheelEvent->delta < 0) {
+          //     sizex -= 1;
+          //     sizey -= 1;
+          //   }
           //
+          //   if (sizex < 1) {
+          //     sizex = 1;
+          //   } else if (sizex > mapSize.x) {
+          //     sizex = mapSize.x;
+          //   }
+          //   if (sizey < 1) {
+          //     sizey = 1;
+          //   } else if (sizey > mapSize.y) {
+          //     sizey = mapSize.y;
+          //   }
           //
-          //   if (sizex<1){sizex = 1;}
-          //   else if(sizex > mapSize.x ){sizex = mapSize.x;}
-          //   if (sizey<1){sizey = 1;}
-          //   else if(sizey > mapSize.y ){sizey = mapSize.y;}
-          //
-          //   size =  sf::Vector2u(static_cast<unsigned
-          //   int>(std::round(sizex)), static_cast<unsigned
-          //   int>(std::round(sizey)));
-          //   project->getToolBar().getSelectedTool()->setSize(size.x,size.y);
+          //   size = sf::Vector2u(static_cast<unsigned int>(std::round(sizex)),
+          //                       static_cast<unsigned
+          //                       int>(std::round(sizey)));
+          //   project->getToolBar().getSelectedTool()->setSize(size.x, size.y);
           // }
         } else {
           if (!gui.getWidgetBelowMouseCursor(wheelEvent->position, true))
