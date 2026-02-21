8c2a16a3f7c5790161c1135ec45161bf3cdf7d4b
Samy <scontuliano@gmail.com>
2026-02-21 13:15:05 +0100

    Divided displayWindow.cpp according to each state(Login, Menu, Game) with proper files
    Login functions in windowLogin.cpp
    Menu functions in windowMenu.cpp
    Game functions in windowGame.cpp
    Project funtions in windowProject.cpp

---
+++ src/Window/windowProject.cpp
@@ -0,0 +1,126 @@
+#include "displayWindow.hpp"
+
+void Window::initDataWidget() {
+  // Grey Backgroung
+  auto background = tgui::Panel::create();
+  background->setSize("100%", "100%");
+  background->getRenderer()->setBackgroundColor({40, 40, 40, 150});
+  gui.add(background, "background");
+  background->setTextSize(30);
+
+  // Box for project's data
+  auto data = tgui::Panel::create();
+  data->setSize("35%", "40%");
+  data->setPosition("50%-17.5%", "50%-20% ");
+  data->getRenderer()->setBackgroundColor(tgui::Color(40, 40, 40));
+  background->add(data, "dataPanel");
+
+  auto title = tgui::Label::create("Nouveau projet");
+  title->getRenderer()->setTextColor(tgui::Color::White);
+  title->setPosition("37%", "5%");
+  title->setTextSize(50);
+  data->add(title);
+
+  auto name = tgui::EditBox::create();
+  name->setSize("80%", "12%");
+  name->setPosition("10%", "20%");
+  name->setDefaultText("Nom: ...");
+  name->getRenderer()->setRoundedBorderRadius(8);
+  name->getRenderer()->setBackgroundColor(tgui::Color(28, 28, 36));
+  name->getRenderer()->setBorderColor(sf::Color(55, 55, 70));
+  name->getRenderer()->setBackgroundColorFocused(tgui::Color::White);
+  data->add(name);
+
+  auto scale = tgui::EditBox::create();
+  scale->setInputValidator("[0-9]*");
+  scale->setMaximumCharacters(5);
+  scale->setSize("80%", "12%");
+  scale->setPosition("10%", "42%");
+  scale->setDefaultText("Echelle: ...");
+  scale->getRenderer()->setRoundedBorderRadius(8);
+  scale->getRenderer()->setBackgroundColor(tgui::Color(28, 28, 36));
+  scale->getRenderer()->setBorderColor(sf::Color(55, 55, 70));
+  scale->getRenderer()->setBackgroundColorFocused(tgui::Color::White);
+  data->add(scale);
+
+  auto sizePx = tgui::EditBox::create();
+  sizePx->setInputValidator("[0-9]*");
+  sizePx->setMaximumCharacters(5);
+  sizePx->setSize("80%", "12%");
+  sizePx->setPosition("10%", "64%");
+  sizePx->setDefaultText("Taille (x): ...");
+  sizePx->getRenderer()->setRoundedBorderRadius(8);
+  sizePx->getRenderer()->setBackgroundColor(tgui::Color(28, 28, 36));
+  sizePx->getRenderer()->setBorderColor(sf::Color(55, 55, 70));
+  sizePx->getRenderer()->setBackgroundColorFocused(tgui::Color::White);
+  data->add(sizePx);
+
+  auto sizePy = tgui::EditBox::create();
+  sizePy->setInputValidator("[0-9]*");
+  sizePy->setMaximumCharacters(5);
+  sizePy->setSize("40%", "12%");
+  sizePy->setPosition("50%", "64%");
+  sizePy->setDefaultText("Taille (y): ...");
+  sizePy->getRenderer()->setRoundedBorderRadius(8);
+  sizePy->getRenderer()->setBackgroundColor(tgui::Color(28, 28, 36));
+  sizePy->getRenderer()->setBorderColor(sf::Color(55, 55, 70));
+  sizePy->getRenderer()->setBackgroundColorFocused(tgui::Color::White);
+  data->add(sizePy);
+
+  auto valid = tgui::Button::create("Valider");
+  valid->setSize("40%", "12%");
+  valid->setPosition("30%", "90% - 20");
+  valid->getRenderer()->setBackgroundColor(sf::Color(99, 102, 241));
+  valid->getRenderer()->setBackgroundColorHover(sf::Color(118, 120, 255));
+  valid->getRenderer()->setTextColor(sf::Color::White);
+  valid->getRenderer()->setBorders(0);
+  valid->getRenderer()->setRoundedBorderRadius(8);
+  data->add(valid);
+  // valid->onPress([this, background]() {
+  //   gui.remove(background);
+  //   createProj();
+  //   // createProj(unsigned int scale, sf::Vector2u size, std::string name,
+  //   //                     unsigned int id, sf::RenderWindow &window,
+  //   //                     tgui::Gui &gui);
+  // });
+  valid->onPress([this, scale, sizePx, sizePy, name]() {
+    // Les getText() sont appelés au moment du clic !
+    this->createProj(scale->getText(), sizePx->getText(), sizePy->getText(),
+                     name->getText(), 0, this->mainWindow, this->gui);
+  });
+  // BUG: Need to return values to create Project
+
+  // return Project{scale->getText(), sf::Vector2u{sizeX, sizeY},
+  // name->getText(),
+  //                &this->mainWindow, &this->gui};
+}
+
+ProjectData Window::askProjectData() {
+  initDataWidget();
+  // ProjectData newProj = initDataWidget();
+  // projectList.push_back(newProj);
+  return ProjectData{};
+}
+
+void Window::createProj(tgui::String scale, tgui::String sizeX,
+                        tgui::String sizeY, tgui::String name, unsigned int id,
+                        sf::RenderWindow &window, tgui::Gui &gui) {
+  // NOTE: Need verification if data isn't empty
+  unsigned int scaleInt = scale.toUInt();
+  sf::Vector2u size = {sizeX.toUInt(), sizeY.toUInt()};
+  std::string nameS = name.toStdString();
+  // Notify the server that a Proj is being created by using
+  // ClientNetworkManager
+  manager.createProject("Owner", size, scaleInt);
+  gui.removeAllWidgets();
+
+  this->project =
+      std::make_unique<Project>(scaleInt, size, nameS, id, mainWindow, gui);
+
+  projectList.push_back(getProjectData(project));
+  setState(projectState::GAME);
+}
+
+ProjectData Window::getProjectData(std::unique_ptr<Project> &newProj) {
+  return ProjectData{newProj->getId(), 0, newProj->getName()};
+}
