fda9df5cfbf323e4f24e1d2fe035e50a106848cd
naimbenziani <naim599100@gmail.com>
2026-02-25 12:33:11 +0100

    install.sh build essential

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 echo "--------------Installation des dépendances------------------------"
 sudo apt update
 sudo apt upgrade
-sudo apt install cmake libfreetype6-dev libx11-dev libxrandr-dev libxcursor-dev libudev-dev libgl1-mesa-dev libflac-dev libogg-dev libvorbis-dev libopenal-dev libxi-dev
+sudo apt install cmake libfreetype6-dev libx11-dev libxrandr-dev libxcursor-dev libudev-dev libgl1-mesa-dev libflac-dev libogg-dev libvorbis-dev libopenal-dev libxi-dev build-essential
 
 rm -rf build
 mkdir build
