1a5ee14aebbe6972a672d8f4bd9ebb3d782b5e6a
Chinsouu <adiya.chinzorig@gmail.com>
2026-03-03 23:07:00 +0100

    git reset --hard HEAD~1

--- src/server/worker.cpp
+++ src/server/worker.cpp
@@ -1,35 +1,84 @@
 #include "worker.hpp"
 #include <iostream>
 
-Worker::Worker(MessageQueue& d_queue, ReponseQueue& r_queue) 
-    : dem_queue(d_queue), rep_queue(r_queue), m_running(true) {}
+Worker::Worker(MutexQueue<IMessage>& demQueue, MutexQueue<Reponse>& repQueue) 
+    : demQueue_(demQueue), repQueue_(repQueue), mRunning_(true) {}
 
 void Worker::run() {
-    
-    while (m_running) {
-    std::shared_ptr<Message> request = dem_queue.pop();
+    std::cout << "[Worker] Démarré et en attente de messages..." << std::endl;
+    std::unique_ptr<IMessage> request;
+    while (mRunning_) {
+        request = demQueue_.pop();
+        std::cout << "[Worker] pop()..." << std::endl;
 
         if (request) {
+            std::cout << "[Worker] Traitement d'un nouveau message..." << std::endl;
             request->process(*this);
         } else {
             break;
         }
-        sf::sleep(sf::milliseconds(1000)); 
     }
 }
 
 
 void Worker::stop() {
-    m_running = false;
-    rep_queue.stop(); 
-    dem_queue.stop();
+    mRunning_ = false;
+    repQueue_.stop(); 
+    demQueue_.stop();
+}
+
+
+void Worker::pushNetwork(std::unique_ptr<Reponse> rps) {
+    repQueue_.push(std::move(rps));
 }
 
 
 long long Worker::verifyLogin(const std::string& pseudo, const std::string& password) {
-    return db_Manager.verifyLogin(pseudo, password);
+    return dbManager_.verifyLogin(pseudo, password);
 }
 
 long long Worker::addUser(const std::string& pseudo, const std::string& password) {
-    return db_Manager.addUser(pseudo, password);
+    return dbManager_.addUser(pseudo, password);
+}
+
+bool Worker::addLink(const long long userId, const long long projectId, const int8_t role) {
+    return dbManager_.addLink(userId, projectId, role);
+}
+
+bool Worker::changeRole(const long long userId, const long long projectId, const int8_t role) {
+    return dbManager_.changeRole(userId, projectId, role);
+}
+
+long long Worker::addProjectSQL(const std::string& name, const long long userId) {
+    return dbManager_.addProject(name, userId);
+}
+
+std::vector<MemberEntry> Worker::getProjectMembers(const long long projectId) {
+    return dbManager_.getProjectMembers(projectId);
+}
+
+std::vector<ProjectEntry> Worker::getUserProjects(const long long userId) {
+    return dbManager_.getUserProjects(userId);
+}
+    
+int8_t Worker::getRole(const long long userId, const long long projectId) {
+    return dbManager_.getRole(userId, projectId);
+}
+
+std::vector<ProjectEntry> Worker::getAllProjects() {
+    return dbManager_.getAllProjects();
+}
+
+
+
+bool Worker::createProjectJson(int projectId, const std::string &projectName, int width, int height, uint scale) {
+    return projManager_.createProjectJson(projectId, QString::fromStdString(projectName), width, height, scale);
+}
+    
+QJsonObject Worker::loadProjectJson(int projectId) {
+    return projManager_.loadProjectJson(projectId);
+}
+
+bool Worker::saveImage(int projectId, const std::string &fileName, const QByteArray &data) {
+    return projManager_.saveImage(projectId, QString::fromStdString(fileName), data);
 }
