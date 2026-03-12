fabe7def86cd7fb14a7efd5e8ff2ae3ff745106c
naimbenziani <naim599100@gmail.com>
2026-03-12 22:07:40 +0100

    fix server for macos

--- src/server/main.cpp
+++ src/server/main.cpp
@@ -1,13 +1,15 @@
 #include "servernetwork.hpp"
 #include "worker.hpp"
 #include <thread>
+#include <QCoreApplication>
 
-int main() {
+int main(int argc, char *argv[]) {
+    QCoreApplication app(argc, argv);
     MutexQueue<IMessage> mesQ;
     MutexQueue<Reponse> repQ;
     ServerNetworkManager servManager(mesQ, repQ);
     Worker worker(mesQ, repQ);
-    
+
     std::thread networkThread(&ServerNetworkManager::run, &servManager);
     std::cout << "[Main] Réseau lancé dans un thread." << std::endl;
 
