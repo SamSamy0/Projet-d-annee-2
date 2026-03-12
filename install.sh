1f901fc7881290405ff8b7fbede22a7acc793e76
naimbenziani <naim599100@gmail.com>
2026-03-12 22:54:19 +0100

    cleaning of the repository

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 echo "--------------Installation des dépendances------------------------"
 sudo apt update
 sudo apt upgrade
-sudo apt install cmake libfreetype6-dev libx11-dev libxrandr-dev libxcursor-dev libudev-dev libgl1-mesa-dev libflac-dev libogg-dev libvorbis-dev libopenal-dev libxi-dev build-essential
+sudo apt install cmake libfreetype6-dev libx11-dev libxrandr-dev libxcursor-dev libudev-dev libgl1-mesa-dev libflac-dev libogg-dev libvorbis-dev libopenal-dev libxi-dev build-essential qt6-base-dev
 
 cd build
 shopt -s extglob
