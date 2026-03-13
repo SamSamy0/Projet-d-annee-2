cfd82428881129bab7b72d7ac644b12abc7113cd
naimbenziani <naim599100@gmail.com>
2026-03-13 21:26:23 +0100

    assetmanager begin

--- src/project/assetmanager.cpp
+++ src/project/assetmanager.cpp
@@ -1,1 +1,52 @@
 #include "assetmanager.hpp"
+#include <iostream>
+#include <QJsonObject>
+#include <QDir>
+#include <QFile>
+#include <QJsonDocument>
+#include <QFileInfo>
+#include <QDebug>
+#include <QJsonArray>
+#include <QString>
+
+
+
+
+
+void AssetManager::loadFromJson(){
+
+  Qfile file(Qstring::fromStdString("../res/sprites/sprites.json"));
+
+
+  if(!file.open(QIODevice::ReadoOnly || QIODevice::Text)){
+    std::cerr << "Erreur : Impossible to open the JSON file" << std::endl;
+        return;
+  }
+  QByteArray fileData = file.readAll();
+  file.close;
+
+  QJsonParseError parseError;
+  QJsonDocumment jsonDoc  = QJsonDocument::fromjson(fileData,&parseError);
+
+  if(parseError.error != QJsonParseError::NoError){
+    std::cerr <<"Synthax Error JSON"<<std::endl;
+    return;
+  }
+
+  QJsonArray jsonArray = jsonDoc.array();
+
+  for(QjsonValue& value : jsonArray){
+
+
+
+
+  }
+
+
+
+
+
+
+
+}
+
