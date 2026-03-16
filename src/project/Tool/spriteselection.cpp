1fdfc4f2c21055e7222d2c13479b75bbdb7f5e0e
naimbenziani <naim599100@gmail.com>
2026-03-16 19:50:11 +0100

    SpriteSelection part 1

--- src/project/Tool/spriteselection.cpp
+++ src/project/Tool/spriteselection.cpp
@@ -1,1 +1,18 @@
 #include "spriteselection.hpp"
+#include <memory>
+
+
+ SpriteSelection::SpriteSelection(std::shared_ptr<Map> map) : Tool(map){}
+
+
+
+bool SpriteSelection::isSelected(uint64_t id)const{
+  if(std::find(selected_.begin(),selected_.end(),id) != selected_.end()){return true;}
+  else{return false;}
+}
+
+void SpriteSelection::clearSelection(){selected_.clear();}
+
+void SpriteSelection::onPress(sf::Vector2i pos){
+
+}
--- src/project/Tool/spriteselection.hpp
+++ src/project/Tool/spriteselection.hpp
@@ -1,6 +1,24 @@
 #pragma once
 #include "tool.hpp"
+#include "../Layer/spritelayer.hpp"
+#include <memory>
+
 
 class SpriteSelection : public Tool {
-  std::vector<sf::Sprite> selection_;
+
+  enum SelectionStatut{
+    NONE,
+    SELECTION,
+    DRAGING
+  };
+
+  std::vector<uint64_t> selected_;
+  bool isSelected(uint64_t id)const;
+public:
+  SpriteSelection(std::shared_ptr<Map> map);
+  void deleteSelectedSprite();
+  void clearSelection();
+  void onPress(sf::Vector2i pos) override;
+  void onDrag(sf::Vector2i pos)override;
+  void onRelease() override;
 };
