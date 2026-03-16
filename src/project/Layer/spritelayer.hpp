03de754ec401c55f36f8f1fbebabf58cdc800a5a
naimbenziani <naim599100@gmail.com>
2026-03-16 17:21:13 +0100

    Id for sprites -> SpriteObject

--- src/project/Layer/spritelayer.hpp
+++ src/project/Layer/spritelayer.hpp
@@ -1,16 +1,24 @@
 #pragma once
 #include "layer.hpp"
+#include <cstdint>
+
+struct SpriteObject{
+  sf::Sprite sprite;
+  uint64_t id;
+  SpriteObject(const sf::Sprite& sprite, uint64_t id);
+};
 
 class SpriteLayer : public Layer {
-  std::vector<sf::Sprite> sprites_;
+  std::vector<SpriteObject> sprites_;
   sf::Vector2i offset_;
+  uint64_t nextId;
 
 public:
   SpriteLayer(std::string name, sf::Vector2u size);
   sf::Vector2i getOffset() const override;
-  std::vector<sf::Sprite> &getSprites();
-  void draw(sf::Sprite &s);
-  void erase(int i);
+  const std::vector<SpriteObject> &getSprites()const;
+  void draw(const sf::Sprite &s);
+  void erase(uint64_t id);
   void shift(sf::Vector2i v) override;
   void drawLayer(sf::RenderTarget &target) override;
 };
--- src/project/Tool/spriteeraser.cpp
+++ src/project/Tool/spriteeraser.cpp
@@ -55,17 +55,15 @@ void SpriteEraser::paint(sf::Vector2i pos) {
   if (!spritelayer)
     return;
 
-  std::vector<sf::Sprite> &sprites = spritelayer->getSprites();
+  /*I had two choices : use the id or the index of the sprite
+   * i chosed to use the id because its cleaner more logical even if complexity is higher */
+  const std::vector<SpriteObject> &sprites = spritelayer->getSprites();
   pos -= spritelayer->getOffset();
-  bool hit = false;
-  int depth;
 
   for (int i = sprites.size() - 1; i >= 0; i--) {
-    if (checkColision(pos, sprites[i].getGlobalBounds())) {
-      hit = true;
-      depth = i;
+    if (checkColision(pos, sprites[i].sprite.getGlobalBounds())) {
+      spritelayer->erase(sprites[i].id);
       break;
     }
   }
-  if (hit){spritelayer->erase(depth);}
 }
