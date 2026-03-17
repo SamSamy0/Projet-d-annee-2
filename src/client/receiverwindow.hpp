dc8acff7fed4f1b44b9029c3eb95920927fcfb53
naimbenziani <naim599100@gmail.com>
2026-03-17 20:07:51 +0100

    declaration of sync receiverWindow

--- src/client/receiverwindow.hpp
+++ src/client/receiverwindow.hpp
@@ -21,6 +21,14 @@ public:
   void addProjectToList(ProjectData projet);
   void updateProjectNameInList(long long id, const std::string &name);
   void updateCreatedProjectId(uint32_t ProjectId);
+  void drawSquare(uint64_t layer_id, int pos_x,int pos_y, float size, uint8_t r, uint8_t g,uint8_t b, uint8_t a);
+  void drawCircle(uint64_t layer_id,int pos_x,int pos_y,float size, uint8_t r, uint8_t g,uint8_t b, uint8_t a);
+  void drawDiamond(uint64_t layer_id,int pos_x,int pos_y,float size_x, uint8_t r, uint8_t g,uint8_t b, uint8_t a);
+  void eraseSquare(uint64_t layer_id,int pos_x,int pos_y,float size);
+  void eraseCircle(uint64_t layer_id,int pos_x,int pos_y,float size);
+  void eraseDiamond(uint64_t layer_id,int pos_x,int pos_y,float size_x,float size_y);
+    
+
   void setState();
 
   // Method on Project
