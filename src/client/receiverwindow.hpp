a94fa12d51c558c04152dc0f38cad64d618dd4fc
naimbenziani <naim599100@gmail.com>
2026-03-18 15:14:52 +0100

    begin of sync shiftLayer

--- src/client/receiverwindow.hpp
+++ src/client/receiverwindow.hpp
@@ -20,8 +20,10 @@ public:
   void drawPixelBrush(uint layer_id, int pos_x, int pos_y, uint8_t r, uint8_t g,
                       uint8_t b, uint8_t a, Shape shape, bool eraser,
                       float size_x, float size_y);
+  void shiftLayer(uint layer_id, int delta_x, int delta_y);
 
   void setState();
   void addProjectData(unsigned int scale, sf::Vector2u size, std::string name,
                       uint id);
 };
+
--- src/project/Tool/shift.cpp
+++ src/project/Tool/shift.cpp
@@ -19,11 +19,22 @@ void Shift::onDrag(sf::Vector2i pos) {
 
   sf::Vector2i delta = pos - lastPos_;
   if (delta.x != 0 || delta.y != 0) {
-    map_->getCurrentLayer()->shift(delta);
+    shiftSender(delta);
   }
   lastPos_ = pos;
+}
+
+void Shift::shift(sf::Vector2i delta){
+  map_->getCurrentLayer()->shift(delta);
+}
+
+void Shift::shiftSender(sf::Vector2i delta){
+  shift(delta);
+  manager_.shiftLayer(map_->getId(),map_->getCurrentLayer()->getId(),delta.x,delta.y);
+
 }
 
 void Shift::onRelease() { isDrawing_ = false; }
 
 Shift::~Shift() {}
+
