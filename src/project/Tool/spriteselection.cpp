bab848c2ebc2eca73b99c357996300f483ded6e6
Naïm Benziani <naim.benziani@ulb.be>
2026-04-20 19:14:39 +0200

    findCenter function (spriteSelection)

--- src/project/Tool/spriteselection.cpp
+++ src/project/Tool/spriteselection.cpp
@@ -48,7 +48,9 @@ void SpriteSelection::onPress(sf::Vector2i pos) {
     if (hit) {
       if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::LControl)) {
         if (isSelected(hitedId)) {
-          selected_.erase(std::remove(selected_.begin(),selected_.end(),hitedId),selected_.end()); //remove the id from selection
+          selected_.erase(
+              std::remove(selected_.begin(), selected_.end(), hitedId),
+              selected_.end()); // remove the id from selection
         } else {
           selected_.push_back(hitedId);
         }
@@ -60,7 +62,7 @@ void SpriteSelection::onPress(sf::Vector2i pos) {
       }
       state_ = SelectionState::DRAGING;
     } else {
-      if (!sf::Keyboard::isKeyPressed(sf::Keyboard::Key::LControl)){
+      if (!sf::Keyboard::isKeyPressed(sf::Keyboard::Key::LControl)) {
         selected_.clear();
       }
       state_ = SelectionState::SELECTION;
@@ -72,65 +74,94 @@ void SpriteSelection::onPress(sf::Vector2i pos) {
 void SpriteSelection::onDrag(sf::Vector2i pos) {
   std::shared_ptr<Layer> layer = map_->getCurrentLayer();
   if (layer->getType() == SPRITELAYER) {
-    std::shared_ptr<SpriteLayer> spritelayer = static_pointer_cast<SpriteLayer>(layer);
+    std::shared_ptr<SpriteLayer> spritelayer =
+        static_pointer_cast<SpriteLayer>(layer);
     pos -= spritelayer->getOffset();
-    sf::Vector2i delta = sf::Vector2i(pos.x -lastPos_.x,pos.y- lastPos_.y);
+    sf::Vector2i delta = sf::Vector2i(pos.x - lastPos_.x, pos.y - lastPos_.y);
 
-
-    if(state_ == SelectionState::DRAGING){
-      for(uint id : selected_){
-        spritelayer->shiftSprite(id,delta);
-        
+    if (state_ == SelectionState::DRAGING) {
+      for (uint id : selected_) {
+        spritelayer->shiftSprite(id, delta);
       }
     }
-      lastPos_ = pos;
-    }
-
+    lastPos_ = pos;
   }
-
+}
 
 void SpriteSelection::onRelease() {
   std::shared_ptr<Layer> layer = map_->getCurrentLayer();
   if (layer->getType() == SPRITELAYER) {
-    std::shared_ptr<SpriteLayer> spritelayer = static_pointer_cast<SpriteLayer>(layer);
+    std::shared_ptr<SpriteLayer> spritelayer =
+        static_pointer_cast<SpriteLayer>(layer);
 
-    if(state_ == SelectionState::SELECTION){
+    if (state_ == SelectionState::SELECTION) {
       sf::FloatRect selectionRect;
 
+      selectionRect.size.x = std::abs(startSelectionPos_.x - lastPos_.x);
+      selectionRect.size.y = std::abs(startSelectionPos_.y - lastPos_.y);
 
-      selectionRect.size.x = std::abs(startSelectionPos_.x-lastPos_.x);
-      selectionRect.size.y = std::abs(startSelectionPos_.y-lastPos_.y);
-
-      //top left corner of the Rectangle
-      selectionRect.position.x =std::min(startSelectionPos_.x,lastPos_.x)  ;
-      selectionRect.position.y = std::min(startSelectionPos_.y,lastPos_.y);
+      // top left corner of the Rectangle
+      selectionRect.position.x = std::min(startSelectionPos_.x, lastPos_.x);
+      selectionRect.position.y = std::min(startSelectionPos_.y, lastPos_.y);
 
       const std::vector<SpriteObject> &sprites = spritelayer->getSprites();
 
       bool isCtrl = sf::Keyboard::isKeyPressed(sf::Keyboard::Key::LControl);
 
-      if(!isCtrl){
+      if (!isCtrl) {
         selected_.clear();
       }
-      for(const auto& sprite : sprites){
-        if(selectionRect.findIntersection(sprite.sprite.getGlobalBounds())){
-          if(isCtrl){
-            if(!isSelected(sprite.id))
-              selected_.push_back(sprite.id); //WARNING: ATTENTION AU COMPORTEMENT SI ON PRESSE CTRL ON CLICK PUIS ON LE RELACHE AVANT ON RELEASE
-            else{
-              selected_.erase(std::remove(selected_.begin(),selected_.end(),sprite.id),selected_.end());
+      for (const auto &sprite : sprites) {
+        if (selectionRect.findIntersection(sprite.sprite.getGlobalBounds())) {
+          if (isCtrl) {
+            if (!isSelected(sprite.id))
+              selected_.push_back(
+                  sprite
+                      .id); // WARNING: ATTENTION AU COMPORTEMENT SI ON PRESSE
+                            // CTRL ON CLICK PUIS ON LE RELACHE AVANT ON RELEASE
+            else {
+              selected_.erase(
+                  std::remove(selected_.begin(), selected_.end(), sprite.id),
+                  selected_.end());
             }
-          }
-          else
+          } else
             selected_.push_back(sprite.id);
-          }
         }
+      }
     }
 
-    else if(state_ == SelectionState::DRAGING){
-
+    else if (state_ == SelectionState::DRAGING) {
     }
-  isDrawing_ = false;
+    isDrawing_ = false;
   }
   state_ = SelectionState::NONE;
+}
+
+sf::Vector2f SpriteSelection::findCenter() {
+  sf::Vector2f res = sf::Vector2f(0, 0);
+  std::vector<SpriteObject> sprites;
+
+  if (map_->getCurrentLayer()->getType() == SPRITELAYER)
+    sprites =
+        static_pointer_cast<SpriteLayer>(map_->getCurrentLayer())->getSprites();
+
+  for (SpriteObject &sprite : sprites) {
+    for (uint id : selected_) {
+      if (sprite.id == id)
+        res += sprite.sprite.getPosition();
+    }
+  }
+  res.x /= selected_.size();
+  res.y /= selected_.size();
+
+  return res;
+}
+void SpriteSelection::rotateSprite(uint spriteId, float angle,
+                                   sf::Vector2f pivot) {
+  
+}
+
+void SpriteSelection::resizeSprite(uint spriteId, float delta,
+                                   sf::Vector2f pivot) {
+
 }
--- src/project/Tool/spriteselection.hpp
+++ src/project/Tool/spriteselection.hpp
@@ -2,8 +2,7 @@
 #include "tool.hpp"
 #include <memory>
 
-
-enum class SelectionState{
+enum class SelectionState {
   NONE,
   SELECTION,
   DRAGING,
@@ -12,18 +11,22 @@ enum class SelectionState{
 };
 class SpriteSelection : public Tool {
 
-
   std::vector<uint> selected_;
   SelectionState state_ = SelectionState::NONE;
   sf::Vector2i startSelectionPos_;
-  bool isSelected(uint)const;
+
+  bool isSelected(uint) const;
+  sf::Vector2f findCenter();
+
 public:
-  SpriteSelection(std::shared_ptr<Map> map, ClientNetworkManager& manager);
-  std::vector<uint>& getSelection();
+  SpriteSelection(std::shared_ptr<Map> map, ClientNetworkManager &manager);
+  std::vector<uint> &getSelection();
   const SelectionState getState() const;
   void deleteSelectedSprite();
   void clearSelection();
   void onPress(sf::Vector2i pos) override;
-  void onDrag(sf::Vector2i pos)override;
+  void onDrag(sf::Vector2i pos) override;
   void onRelease() override;
+  void rotateSprite(uint spriteId, float angle, sf::Vector2f pivot);
+  void resizeSprite(uint spriteId, float delta, sf::Vector2f pivot);
 };
