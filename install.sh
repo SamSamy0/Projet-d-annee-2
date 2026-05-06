6dd83d6658fd82d96b1dcd3b33d8ca157e42a04d
naimbenziani <naim599100@gmail.com>
2026-05-06 19:14:13 +0200

    modification to compille on ubuntu

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 echo "--------------Installation des dépendances------------------------"
 sudo apt update
 sudo apt upgrade
-sudo apt install cmake libfreetype6-dev libx11-dev libxrandr-dev libxcursor-dev libudev-dev libgl1-mesa-dev libflac-dev libogg-dev libvorbis-dev libopenal-dev libxi-dev build-essential qt6-base-dev
+sudo apt install cmake libfreetype6-dev libx11-dev libxrandr-dev libxcursor-dev libudev-dev libgl1-mesa-dev libflac-dev libogg-dev libvorbis-dev libopenal-dev libxi-dev build-essential qt6-base-dev qt6-tools-dev
 
 cd build
 shopt -s extglob
--- src/server/LayerManager.hpp
+++ src/server/LayerManager.hpp
@@ -2,6 +2,7 @@
 #include <unordered_map>
 #include <list>
 #include <QJsonArray>
+#include <QImage>
 #include "SpriteLayer.hpp"
 
 struct Layer {
